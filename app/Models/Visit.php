af26b6e51c0fe4039a94f0e2b1d8cd67258c6b6d
Christian Dominguez <christian@redbonzai.com>
2018-10-31 00:47:22 +0000

    small updates to Vist.php, and added generated view files to .gitignore

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -8,9 +8,11 @@ class Visit extends Model
 {
     protected $table = 'visit';
     protected $primaryKey = 'visit_id';
+
     protected $fillable = [
         'visit_id', 'visit_date', 'visitcol','clinic_id','provider_id', 'specialty_id', 'patient_id'
-            ];
+    ];
+
     public $timestamps = false;
 }
 
